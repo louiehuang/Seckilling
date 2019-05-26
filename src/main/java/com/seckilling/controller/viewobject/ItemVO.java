92527c329e6ec591a7dcc287374a920571280fa8
louiehuang <louiehuang21@gmail.com>
2019-05-26 14:57:23 -0700

    rename imgUrl in the item table to img_url

--- src/main/java/com/seckilling/controller/viewobject/ItemVO.java
+++ src/main/java/com/seckilling/controller/viewobject/ItemVO.java
@@ -10,4 +10,60 @@ public class ItemVO {
     private BigDecimal price;
     private Integer stock;
     private Integer sales;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
 }
