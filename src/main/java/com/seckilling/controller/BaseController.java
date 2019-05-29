5ccf6365981895a1918be22f1f8fd8f1b1149b9e
louiehuang <louiehuang21@gmail.com>
2019-05-29 14:54:42 -0700

    handle 404 and binding exception

--- src/main/java/com/seckilling/controller/BaseController.java
+++ src/main/java/com/seckilling/controller/BaseController.java
@@ -15,21 +15,21 @@ import java.util.Map;
 public class BaseController {
     public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
 
-    // Handle exceptions that are thrown from controller layer
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception ex) {
-        Map<String, Object> responseData = new HashMap<>();
-        if (ex instanceof BusinessException) {
-            BusinessException businessException = (BusinessException) ex;
-            responseData.put("errCode", businessException.getErrCode());
-            responseData.put("errMsg", businessException.getErrMsg());
-        } else {
-            responseData.put("errCode", EBusinessError.UNKNOWN_ERROR.getErrCode());
-            responseData.put("errMsg", EBusinessError.UNKNOWN_ERROR.getErrMsg());
-        }
-        return CommonReturnType.create(responseData, "fail");
-    }
+//    // Handle exceptions that are thrown from controller layer
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Object handlerException(HttpServletRequest request, Exception ex) {
+//        Map<String, Object> responseData = new HashMap<>();
+//        if (ex instanceof BusinessException) {
+//            BusinessException businessException = (BusinessException) ex;
+//            responseData.put("errCode", businessException.getErrCode());
+//            responseData.put("errMsg", businessException.getErrMsg());
+//        } else {
+//            responseData.put("errCode", EBusinessError.UNKNOWN_ERROR.getErrCode());
+//            responseData.put("errMsg", EBusinessError.UNKNOWN_ERROR.getErrMsg());
+//        }
+//        return CommonReturnType.create(responseData, "fail");
+//    }
 
 }
